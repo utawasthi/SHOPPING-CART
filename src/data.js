85bce293cdf7cc354b5bf35a4f560cbcb1ff19fb
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2024-07-11 11:07:42 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,29 @@
+shopItemsData = [{
+  id : "id1",
+  name :"Casual Shirt ",
+  price : 45,
+  desc : "Casual Shirt Men",
+  img : "images/img1.jpeg"
+
+},
+{
+  id : "id2",
+  name :"Casual Shirt",
+  price : 50,
+  desc : "Casual Shirt Boys",
+  img : "images/img2.jpeg"
+},
+{
+  id : "id3",
+  name :"Hat",
+  price :  45,
+  desc : "Summer Hat",
+  img : "images/img3.jpeg"
+},
+{
+  id : "id4",
+  name :"Gown",
+  price : 700,
+  desc : "Ladies Gown",
+  img : "images/img4.jpeg"
+}]
